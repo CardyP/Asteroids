76c6e14b85aba4e7748f3d777fb64e5ac726a7f8
localyokel86 <paolo.cardarella@gmail.com>
2025-08-10 20:25:40 -0400

    player and player movement/rotation

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()  # Set frame rate to 60 FPS
+    dt = 0
+
+    #Player
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}", f"\nScreen height: {SCREEN_HEIGHT}")
 
@@ -13,9 +20,14 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
-
+        player.draw(screen)
+        player.update(dt)
+        
+        
         pygame.display.flip()
 
+       
 
+        dt = clock.tick(60) / 1000.0  # Convert milliseconds to seconds
 if __name__ == "__main__":
     main()
