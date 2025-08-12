23b1d2f1e631006a0700b849de09b3a5169f2d05
localyokel86 <paolo.cardarella@gmail.com>
2025-08-12 15:12:04 -0400

    asteroids splitting

--- asteroids.py
+++ asteroids.py
@@ -1,28 +1,37 @@
 import pygame
 import random
 from circleshape import CircleShape
-from main import asteroid
 from constants import *
 
 class Asteroids(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        
+        
 
     def split(self):
-        self.kill()  # Remove the current asteroid
+        self.kill()
+
         if self.radius <= ASTEROID_MIN_RADIUS:
             return "Asteroid too small to split"
-        else:
-            rand_angle = random.uniform(20, 50)
-            
-            velocity1 = self.velocity.rotate(rand_angle) * 1.2
-            velocity2 = self.velocity.rotate(-rand_angle) * 1.2
 
-            new_radius = self.radius - ASTEROID_MIN_RADIUS
-            
-            asteroid1 = Asteroids(self.position, new_radius, velocity1)
-            asteroid2 = Asteroids(self.position, new_radius, velocity2)
-            return asteroid1, asteroid2
+        # randomize the angle of the split
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        # Create first asteroid
+        asteroid1 = Asteroids(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = a * 1.2
+        
+        # Create second asteroid
+        asteroid2 = Asteroids(self.position.x, self.position.y, new_radius)
+        asteroid2.velocity = b * 1.2
+
+
         
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
